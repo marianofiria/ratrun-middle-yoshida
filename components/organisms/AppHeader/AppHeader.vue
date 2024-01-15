f2558f4f698f8e2c3236f597f275d707e11ca139
XXXX <XXXX@hogehoge.com>
2024-01-15 14:23:33 +0900

    feat:ヘッダー作成

--- components/organisms/AppHeader/AppHeader.vue
+++ components/organisms/AppHeader/AppHeader.vue
@@ -1,13 +1,22 @@
 <template>
- <div>
-  <p>こちらがヘッダーコンポーネントです</p>
- </div>
+  <header>
+    <ul>
+      <li><a>ホーム</a></li>
+      <li><a>経営理念</a></li>
+      <li><a>事業理念</a></li>
+      <li><a>事業目的</a></li>
+      <li><a>お問い合せ</a></li>
+    </ul>
+  </header>
 </template>
 
-
 <script>
-export default {
-};
+export default {};
 </script>
-<style lang='scss' scoped>
+<style lang="scss" scoped>
+ul {
+  display: flex;
+  list-style: none;
+  gap: 20px;
+}
 </style>
