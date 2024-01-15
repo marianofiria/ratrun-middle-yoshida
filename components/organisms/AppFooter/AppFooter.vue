fe497fa94a84efa432569dda6c1ec73152b483ea
XXXX <XXXX@hogehoge.com>
2024-01-15 13:59:02 +0900

    feat: 初期設定

---
+++ components/organisms/AppFooter/AppFooter.vue
@@ -0,0 +1,13 @@
+<template>
+ <div>
+  <p>こちらがフッターコンポーネントです</p>
+ </div>
+</template>
+
+
+<script>
+export default {
+};
+</script>
+<style lang='scss' scoped>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+ <div>
+   <AppHeader />
+   <NuxtPage />
+   <AppFooter />
+ </div>
+</template>
+
+
+<script>
+
+import { AppHeader } from "../components/organisms/AppHeader";
+import { AppFooter } from "../components/organisms/AppFooter";
+
+export default {
+
+components: {AppHeader,AppFooter}
+
+};
+</script>
+
+<style lang='scss' scoped>
+
+</style>
